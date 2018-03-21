5464a5f793002a14be5d6bb375aca7a65f854ec8
Marko Zorec <mzorec@gmail.com>
2018-03-21 22:48:07 +0100

    Implemented HttpClientFactory

---
+++ net/Infrastucture/HttpClientFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SmartHome.Infrastucture
+{
+    public class HttpClientFactory : IHttpClientFactory
+    {
+        private static IDictionary<string, HttpClient> _clients = new Dictionary<string, HttpClient>();
+
+        public HttpClient Create(string baseUrl)
+        {
+            HttpClient client;
+
+            if (_clients.TryGetValue(baseUrl, out client))
+            {
+                return client;
+            }
+
+            client = new HttpClient
+            {
+                BaseAddress = new Uri(baseUrl),
+            };
+
+            _clients[baseUrl] = client;
+
+            return client;
+        }
+    }
+}
---
+++ net/Infrastucture/IHttpClientFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SmartHome.Infrastucture
+{
+    public interface IHttpClientFactory
+    {
+        HttpClient Create(string baseUrl);
+    }
+}
