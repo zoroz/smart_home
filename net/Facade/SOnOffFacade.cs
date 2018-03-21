a1c3f448b32f564ff45a6edf8b3d8b1db15737a8
Marko Zorec <mzorec@gmail.com>
2018-03-21 23:33:26 +0100

    SonOffFascade now uses rest client.

--- net/Facade/SOnOffFacade.cs
+++ net/Facade/SOnOffFacade.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using SmartHome.Contracts.SOnOff;
+using SmartHome.Infrastucture;
+using SmartHome.Infrastucture.Attributes;
 
 namespace SmartHome.Facade
 {
@@ -18,14 +20,17 @@ namespace SmartHome.Facade
 
     public class SOnOffFacade : ISOnOffFacade
     {
+        private IRestClient _restClient;
+
+        public SOnOffFacade(IRestClientFactory restClientFactory)
+        {
+            _restClient = restClientFactory.Create("https://api.coolkit.cc:8080/api");
+        }
+
+        [Post("user/login")]
         public async Task<LoginResponse> Login(string userName, string password)
         {
-            HttpClient client = new HttpClient(new HttpClientHandler { Proxy = new WebProxy("http://localhost:8888") });
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("sign", "qvpP9CHX/PUccoFpbpRzDPmFe2PVYZ2pATCP/3kzyMk=");
-            //client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip, deflate"));
-            //client.DefaultRequestHeaders.Add("Auth0-Client", "eyJuYW1lIjoiYXV0aDAuanMiLCJ2ZXJzaW9uIjoiOC42LjEifQ==");
-            HttpContent content = new StringContent(JsonConvert.SerializeObject(new LoginRequest
+           return await _restClient.SendAsync<LoginResponse>(new LoginRequest
             {
                 Appid = "oeVkj2lYFGnJu5XUtWisfW4utiN4u9Mq",
                 Nonce = "u2omanuc",
@@ -34,15 +39,7 @@ namespace SmartHome.Facade
                 Ts = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds,
                 Version = 6,
                 Email = userName
-            }), Encoding.UTF8);
-
-            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-            //content.Headers.Add("Origin", "file://");
-            //content.Headers.Add("X-Requested-With", "eu.seltron.clausius");
-
-            HttpResponseMessage res = await client.PostAsync("https://api.coolkit.cc:8080/api/user/login", content);
-            string data = await res.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<LoginResponse>(data);
+            });
         }
     }
 
--- net/Startup.cs
+++ net/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using SmartHome.Facade;
+using SmartHome.Infrastucture;
 using SmartHome.WebSockets;
 using Swashbuckle.AspNetCore.Swagger;
 
@@ -32,6 +33,8 @@ namespace SmartHome
             //services.AddTransient<WebSocketRequestHandler>();
             services.AddSingleton<ISeltronFacade, SeltronFacade>();
             services.AddSingleton<ISOnOffFacade, SOnOffFacade>();
+            services.AddSingleton<IRestClientFactory, RestClientFactory>();
+            services.AddSingleton<IHttpClientFactory, HttpClientFactory>();
             //services.AddTransient<WebSocketMiddleware>();
 
             services.AddSwaggerGen(c =>
