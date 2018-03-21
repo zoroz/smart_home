c54629c2f3665fc49df1c70118d8cdc0fb6a6247
Marko Zorec <mzorec@gmail.com>
2018-03-21 23:24:45 +0100

    Implemented rest client factory.

---
+++ net/Infrastucture/IRestClientFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartHome.Infrastucture
+{
+    public interface IRestClientFactory
+    {
+    }
+}
